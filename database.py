c4f6355d1f4e649ff2c50e335c332aaea4adfc97
Paul Adekomi <pauladekomi@gmail.com>
2026-09-25 01:48:36 +0100

    Refactor database connection setup by removing unused engine_kwargs and simplifying engine creation

--- database.py
+++ database.py
@@ -1,15 +1,12 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
+DATABASE_URL= os.getenv("DATABASE_URL")
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./invoicepal.db")
-engine_kwargs = {}
-if DATABASE_URL.startswith("sqlite"):
-    engine_kwargs["connect_args"] = {"check_same_thread": False}
+engine = create_engine(DATABASE_URL)
 
-engine = create_engine(DATABASE_URL, **engine_kwargs)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
